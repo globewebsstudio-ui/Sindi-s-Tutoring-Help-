1503c64d0bf87ae8e9445e043afe077d7e22abb0
globewebsstudio-ui <globewebsstudio@gmail.com>
2026-02-05 23:54:47 +0200

    script.js
    Signed-off-by: globewebsstudio-ui <globewebsstudio@gmail.com>

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const galleryData = [
     {
         id: 1,
-        image: "assets/images/work4.jpg.jpeg",
+        image: "assets/images/Mitchells-plain-schoolkids.jpg",
         title: "Math Fun with Blocks",
         description: "Learning addition and subtraction with colorful building blocks!",
         category: "math"
